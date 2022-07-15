35511a1d5306f67ed91a6456f0750bb85296ece6
auriwan <auriwan.yasper@sci.ui.ac.id>
2022-07-15 16:58:24 +0700

    auth activity

---
+++ app/src/main/java/com/elthobhy/viefirebaseauth/AuthActivity.kt
@@ -0,0 +1,16 @@
+package com.elthobhy.viefirebaseauth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.elthobhy.viefirebaseauth.databinding.ActivityAuthBinding
+
+class AuthActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAuthBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
