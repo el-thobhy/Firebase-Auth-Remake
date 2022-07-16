a217510a7667497cfc8f6f9d6b7add0232c6703f
auriwan <auriwan.yasper@sci.ui.ac.id>
2022-07-16 17:06:19 +0700

    Sign In Activity Using FirebaseAuth

--- app/src/main/java/com/elthobhy/viefirebaseauth/AuthActivity.kt
+++ app/src/main/java/com/elthobhy/viefirebaseauth/AuthActivity.kt
@@ -4,14 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.elthobhy.viefirebaseauth.databinding.ActivityAuthBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class AuthActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAuthBinding
+    private var currentUser : FirebaseUser? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAuthBinding.inflate(layoutInflater)
+        currentUser = FirebaseAuth.getInstance().currentUser
         setContentView(binding.root)
 
         onClick()
@@ -25,6 +29,14 @@ class AuthActivity : AppCompatActivity() {
             btnSignUpAuth.setOnClickListener {
                 startActivity(Intent(this@AuthActivity,SignUpActivity::class.java))
             }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        currentUser?.let {
+            startActivity(Intent(this,MainActivity::class.java))
+            finishAffinity()
         }
     }
 }
--- app/src/main/java/com/elthobhy/viefirebaseauth/SignInActivity.kt
+++ app/src/main/java/com/elthobhy/viefirebaseauth/SignInActivity.kt
@@ -2,16 +2,26 @@ package com.elthobhy.viefirebaseauth
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.elthobhy.viefirebaseauth.databinding.ActivitySigninBinding
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.EmailAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import java.util.regex.Pattern
 
 class SignInActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySigninBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var dialogLoading: AlertDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySigninBinding.inflate(layoutInflater)
+        firebaseAuth = FirebaseAuth.getInstance()
+        dialogLoading = showDialogLoading(this)
         setContentView(binding.root)
 
         initActionbar()
@@ -21,7 +31,12 @@ class SignInActivity : AppCompatActivity() {
     private fun onCLick() {
         binding.apply {
             btnSignIn.setOnClickListener {
-                startActivity(Intent(this@SignInActivity, MainActivity::class.java))
+                val email = etEmailSignIn.text.toString().trim()
+                val pass = etPasswordSignIn.text.toString().trim()
+                if(validationCheck(email, pass)){
+                    dialogLoading.show()
+                    loginCredential(email, pass)
+                }
             }
             btnForgotPass.setOnClickListener {
                 startActivity(Intent(this@SignInActivity, ForgotPasswordActivity::class.java))
@@ -30,6 +45,51 @@ class SignInActivity : AppCompatActivity() {
                 finish()
             }
         }
+    }
+
+    private fun loginCredential(email: String, pass: String) {
+        val credential = EmailAuthProvider.getCredential(email, pass)
+        signInToServer(credential)
+    }
+
+    private fun signInToServer(credential: AuthCredential) {
+        firebaseAuth.signInWithCredential(credential)
+            .addOnCompleteListener { task->
+                dialogLoading.dismiss()
+                if(task.isSuccessful){
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finishAffinity()
+                }else{
+                    showDialogError(this, "Failed To Login")
+                }
+            }
+            .addOnFailureListener {
+                dialogLoading.dismiss()
+                showDialogError(this,it.message.toString())
+            }
+    }
+
+    private fun validationCheck(email: String, pass: String): Boolean {
+        binding.apply {
+            when{
+                email.isEmpty() -> {
+                    etEmailSignIn.error = "Pleasae Field Your Email"
+                    etEmailSignIn.requestFocus()
+                }
+                !Patterns.EMAIL_ADDRESS.matcher(email).matches() -> {
+                    etEmailSignIn.error = "Please Use Valid Email"
+                    etEmailSignIn.requestFocus()
+                }
+                pass.isEmpty() -> {
+                    etPasswordSignIn.error = "Please Field Your Password"
+                    etPasswordSignIn.requestFocus()
+                }
+                else -> {
+                    return true
+                }
+            }
+        }
+        return false
     }
 
     private fun initActionbar() {
